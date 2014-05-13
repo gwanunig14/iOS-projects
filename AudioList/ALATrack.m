76872e592411f711b64234b23cf80ac2235305e6
T.J. Mercer <thomaskmercerjr@gmail.com>
2014-05-13 09:43:13 -0400

    missing

---
+++ AudioList/ALATrack.m
@@ -0,0 +1,19 @@
+//
+//  ALASong.m
+//  AudioList
+//
+//  Created by T.J. Mercer on 5/10/14.
+//  Copyright (c) 2014 T.J. All rights reserved.
+//
+
+#import "ALATrack.h"
+
+@implementation ALATrack
+
++(id)newTrack
+{
+    return [[ALATrack alloc]init];
+}
+
+@end
+
